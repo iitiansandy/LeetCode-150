b406a4b3a6194a6c30610d43404026f79c99125a
Sandeep Kumar <60264824+iitiansandy@users.noreply.github.com>
2026-01-20 20:40:28 +0530

    product except self problem added

--- Array_String.js
+++ Array_String.js
@@ -661,3 +661,32 @@ function firstNonRepeatingChar(str) {
 }
 
 // console.log(firstNonRepeatingChar("geeksforgeeks")); // 'l'
+
+
+/**
+ * Prob: Product of Array Except Self
+ * Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+ * The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+ * You must write an algorithm that runs in O(n) time and without using the division operation.
+ */
+
+function productExceptSelf(nums) {
+    const n = nums.length;
+    const answer = new Array(n).fill(1);
+
+    let leftProduct = 1;
+    for (let i = 0; i < n; i++) {
+        answer[i] *= leftProduct;
+        leftProduct *= nums[i];
+    }
+
+    let rightProduct = 1;
+    for (let i = n - 1; i >= 0; i--) {
+        answer[i] *= rightProduct;
+        rightProduct *= nums[i];
+    }
+
+    return answer;
+}
+
+console.log(productExceptSelf([1, 2, 3, 4])); // [24, 12, 8, 6]
