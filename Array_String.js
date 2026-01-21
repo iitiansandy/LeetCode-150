e7edf0bf668cf7a18cfc3a6d4cd90701c05527f2
Sandeep Kumar <60264824+iitiansandy@users.noreply.github.com>
2026-01-21 20:39:52 +0530

    gas station problem added

--- Array_String.js
+++ Array_String.js
@@ -689,4 +689,40 @@ function productExceptSelf(nums) {
     return answer;
 }
 
-console.log(productExceptSelf([1, 2, 3, 4])); // [24, 12, 8, 6]
+// console.log(productExceptSelf([1, 2, 3, 4])); // [24, 12, 8, 6]
+
+
+/**
+ * Prob: Gas Station
+ * There are n gas stations along a circular route, where the amount of gas at the ith station is gas[i].
+ * You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from the ith station to its next (i + 1)th station. 
+ * You begin the journey with an empty tank at one of the gas stations.
+ * Given two integer arrays gas and cost, return the starting gas station's index if you can travel around the circuit once in the clockwise direction, 
+ * otherwise return -1. If there exists a solution, it is guaranteed to be unique
+ */
+
+function canCompleteCircuit(gas, cost) {
+    let totalGas = 0;
+    let totalCost = 0;
+    let start = 0;
+    let gasTank = 0;
+
+    for (let i = 0; i < gas.length; i++) {
+        totalGas += gas[i];
+        totalCost += cost[i];
+        gasTank += gas[i] - cost[i];
+
+        if (gasTank < 0) {
+            start = i + 1;
+            gasTank = 0;
+        }
+    }
+
+    if (totalGas < totalCost) {
+        return -1;
+    }
+
+    return start;
+}
+
+console.log(canCompleteCircuit([1, 2, 3, 4, 5], [3, 4, 5, 1, 2])); // 3
