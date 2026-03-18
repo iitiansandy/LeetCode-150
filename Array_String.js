1c8f1836b9e4f83eadcaf56d40b82998773e706f
iitrsandeepblive <sandeepkumar@blive.co.in>
2026-03-18 10:12:44 +0530

    candy problem added

--- Array_String.js
+++ Array_String.js
@@ -726,3 +726,39 @@ function canCompleteCircuit(gas, cost) {
 }
 
 // console.log(canCompleteCircuit([1, 2, 3, 4, 5], [3, 4, 5, 1, 2])); // 3
+
+
+/**
+ * Prob: Candy
+ * There are n children standing in a line. Each child is assigned a rating value given in the integer array ratings.
+ *
+ * You are giving candies to these children subjected to the following requirements:
+ *
+ * Each child must have at least one candy.
+ * Children with a higher rating get more candies than their neighbors.
+ * Return the minimum number of candies you need to have to distribute the candies to the children.
+ */
+
+function candy(ratings) {
+    const n = ratings.length;
+    const candies = new Array(n).fill(1);
+
+    // Left to right pass
+    for (let i = 1; i < n; i++) {
+        if (ratings[i] > ratings[i - 1]) {
+            candies[i] = candies[i - 1] + 1;
+        }
+    }
+
+    // Right to left pass
+    for (let i = n - 2; i >= 0; i--) {
+        if (ratings[i] > ratings[i + 1]) {
+            candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+        }
+    }
+
+    return candies.reduce((sum, candy) => sum + candy, 0);
+}
+
+// console.log(candy([1, 0, 2])); // 5
+// console.log(candy([1, 2, 2])); // 4
