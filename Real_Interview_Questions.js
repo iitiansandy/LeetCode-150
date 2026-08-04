fd2f5576893153d7e05b627b8e01970f9c1aa734
iitiansandy <sandeep.sid.kumar10@gmail.com>
2026-08-05 00:01:15 +0530

    map, filter, reduce polyfills added

--- Real_Interview_Questions.js
+++ Real_Interview_Questions.js
@@ -526,6 +526,27 @@ Array.prototype.myMap = function(callback) {
     return result;
 };
 
+// Implement Array.prototype.filter
+Array.prototype.myFilter = function(callback) {
+    const result = [];
+    for (let i = 0; i < this.length; i++) {
+        if (callback(this[i], i, this)) {
+            result.push(this[i]);
+        }
+    }
+    return result;
+};
+
+// Implement Array.prototype.reduce
+Array.prototype.myReduce = function(callback, initialValue) {
+    let accumulator = initialValue !== undefined ? initialValue : this[0];
+    let startIndex = initialValue !== undefined ? 0 : 1;
+
+    for (let i = startIndex; i < this.length; i++) {
+        accumulator = callback(accumulator, this[i], i, this);
+    }
+    return accumulator;
+}
 
 // Ride Sharing Matching System
 /**
